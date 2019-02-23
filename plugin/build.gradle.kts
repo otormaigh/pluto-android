4908f2883bc50759bb0a05f94f7cb48f3f0eec2e
Elliot Tormey <elliot.tormey@gmail.com>
2019-02-23 22:24:00 +0000

    add Gradle portal plugin publisher

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -4,4 +4,31 @@ repositories {
 
 plugins {
   `kotlin-dsl`
+  `java-gradle-plugin`
+  id("com.gradle.plugin-publish") version "0.10.1"
+}
+
+group = "ie.otormaigh.pluto"
+version = "0.1-${System.currentTimeMillis()}"
+
+gradlePlugin {
+  plugins {
+    create("pluto") {
+      id = "ie.otormaigh.pluto"
+      implementationClass = "ie.otormaigh.pluto.plugin.PlutoPlugin"
+    }
+  }
+}
+
+pluginBundle {
+  website = "https://otormaigh.ie"
+  vcsUrl = "https://github.com/otormaigh/pluto-android"
+  description = "A Gradle plugin toolbox containing helper functions and tasks."
+
+  (plugins) {
+    "pluto" {
+      displayName = "Pluto"
+      tags = listOf("pluto", "toolbox", "kotlin")
+    }
+  }
 }
