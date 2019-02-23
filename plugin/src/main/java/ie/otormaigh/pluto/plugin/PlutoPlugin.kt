7ca4ebecb25716a358945f75e8276a805e9e6074
Elliot Tormey <elliot.tormey@gmail.com>
2019-02-23 22:17:21 +0000

    add basic plugin

---
+++ plugin/src/main/java/ie/otormaigh/pluto/plugin/PlutoPlugin.kt
@@ -0,0 +1,9 @@
+package ie.otormaigh.pluto.plugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class PlutoPlugin : Plugin<Project> {
+  override fun apply(target: Project) {
+  }
+}
