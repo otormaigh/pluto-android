71b0f7fe8b882351695efead097c84f2bea3c9b8
Elliot Tormey <elliot.tormey@gmail.com>
2019-02-24 01:10:08 +0000

    add bintray upload task to plugin module

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,6 +1,15 @@
+import com.novoda.gradle.release.PublishExtension
+
 repositories {
   jcenter()
 }
+
+buildscript {
+  dependencies {
+    classpath("com.novoda:bintray-release:0.9")
+  }
+}
+apply<com.novoda.gradle.release.ReleasePlugin>()
 
 plugins {
   `kotlin-dsl`
@@ -8,27 +17,47 @@ plugins {
   id("com.gradle.plugin-publish") version "0.10.1"
 }
 
-group = "ie.otormaigh.pluto"
-version = "0.1-${System.currentTimeMillis()}"
+val NAME = "pluto"
+val GROUP_ID = "ie.otormaigh.pluto"
+val VERSION = "0.1-${System.currentTimeMillis()}"
+val DESCRIPTION = "A Gradle plugin toolbox containing helper functions and tasks."
+val WEBSITE = "https://otormaigh.ie"
+val VCS_URL = "https://github.com/otormaigh/pluto-android"
+
+group = GROUP_ID
+version = VERSION
 
 gradlePlugin {
   plugins {
-    create("pluto") {
-      id = "ie.otormaigh.pluto"
+    create(NAME) {
+      id = GROUP_ID
       implementationClass = "ie.otormaigh.pluto.plugin.PlutoPlugin"
     }
   }
 }
 
 pluginBundle {
-  website = "https://otormaigh.ie"
-  vcsUrl = "https://github.com/otormaigh/pluto-android"
-  description = "A Gradle plugin toolbox containing helper functions and tasks."
+  website = WEBSITE
+  vcsUrl = VCS_URL
+  description = DESCRIPTION
 
   (plugins) {
-    "pluto" {
-      displayName = "Pluto"
+    NAME {
+      displayName = NAME
       tags = listOf("pluto", "toolbox", "kotlin")
     }
   }
+}
+
+configure<PublishExtension> {
+  bintrayUser = project.properties["bintray.user"] as String
+  bintrayKey = project.properties["bintray.apikey"] as String
+
+  userOrg = "otormaigh"
+  groupId = GROUP_ID
+  artifactId = "pluto-plugin"
+  publishVersion = VERSION
+  desc = DESCRIPTION
+  website = WEBSITE
+  dryRun = false
 }
